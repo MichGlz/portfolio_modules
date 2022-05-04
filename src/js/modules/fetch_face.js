a4f88716bbfe9dd6a8d40917f2f739a16505ca4c
Miguel <migu0403@stud.kea.dk>
2022-05-04 09:32:33 +0200

    add more code to eyes

--- src/js/modules/fetch_face.js
+++ src/js/modules/fetch_face.js
@@ -5,5 +5,13 @@ export default function fetchFace() {
     })
     .then(function (data) {
       document.querySelector(".svg-container").innerHTML = data;
+    })
+    .then(function () {
+      const closeEyes = document.getElementById("close-eyes");
+      const openEyes = document.getElementById("eyes");
+      closeEyes.classList = "eyes";
+      openEyes.classList = "eyes";
+      closeEyes.classList.add("hide");
+      openEyes.classList.add("active");
     });
 }
