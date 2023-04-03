e5018460fc2c367a58560b69c5cb8d1cdb746ea4
Mich Gonzalez <miguel.glz.da@gmail.com>
2023-04-03 22:38:19 +0200

    php contact public

---
+++ public/contact.php
@@ -0,0 +1,22 @@
+<?php
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+  // Get the form data
+  $name = $_POST["name"];
+  $email = $_POST["email"];
+  $message = $_POST["message"];
+
+  // Send an email
+  $to = "hola@michgonzalez.com";
+  $subject = "New message from website contact form";
+  $body = "Name: $name\nEmail: $email\nMessage: $message";
+  $headers = "From: $email";
+
+  if(mail($to, $subject, $body, $headers)) {
+    header("Location: success.html");
+    exit();
+  } else {
+    header("Location: error.html");
+    exit();
+  }
+}
+?>
